34cbcc60abc61d25bf96c42d46de86d75082cef0
quentinctvt <quentin.cutivet@supelec.fr>
2019-08-16 15:07:27 +0900

    Update __init__.py

--- gym_toio/envs/__init__.py
+++ gym_toio/envs/__init__.py
@@ -1,3 +1,4 @@
 from gym_toio.envs.toio_env import ToioEnv
 from gym_toio.envs.toiofewactions_env import ToiofewactionsEnv
 from gym_toio.envs.toiosuccess_env import ToiosuccessEnv
+from gym_toio.envs.toiotimeobstaclesstate_env import ToioTimeObstaclesStateEnv
